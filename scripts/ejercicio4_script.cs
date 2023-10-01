d0c7051c2a5e58c5da8fec958ff8579d218144f6
Alicia G. Cruz Pérez <alu0101420868@ull.edu.es>
2023-10-01 14:21:31 +0100

    Add files via upload

---
+++ scripts/ejercicio4_script.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio_4 : MonoBehaviour
+{
+    void Update()
+    {
+        Debug.Log("Posición de la esfera: " + transform.position);
+    }
+}
